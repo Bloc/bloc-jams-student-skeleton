1c7c78cdb1295179200d30d0231989ca9e12ee71
Robert Smolinski <rob@bloc.io>
2014-04-28 10:56:17 -0700

    Added missing dependence on 'Services'.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,8 @@ require('./services');
 // Top level application module for our website.
 angular.module('BlocJams', [
   'Models',
-  'Controllers'
+  'Controllers',
+  'Services'
 ]).config(function ($compileProvider) {
   // Prevent angular from marking links with a variety of protocols "unsafe"
   $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|webcal|mailto|file|tel):/);
